1ea4727c1b36b00492ad78dde0c7ab301d89eda1
GerardMercade <mercadegerard@gmail.com>
2021-05-26 11:35:23 +0200

    Reanomenem variables i afegim que els gràfics es guardin directament

--- Analisis.R
+++ Analisis.R
@@ -1,15 +1,17 @@
 # Escrivim el nom dels arxius que utilitzarem
-# Agent que hem creat nosaltres
-nom_arxiu_nostre <- "training1_oscar.txt"
+# 1r dels agents analitzats (verd)
+nom_arxiu_1 <- "training1_Oscar.txt"
 
-# Agent de l'arxiu penjat a Atenea
-nom_arxiu_atenea <- "agent_500k_soufi.txt"
+# 2n dels agents analitzats (blau)
+nom_arxiu_2 <- "agent_500k_soufi.txt"
 
 # Indiquem de quin arxiu volem que ens doni detalls 
 # Es mostraran, en concret, els Grafics Reward i Loss
 
-# TRUE és el d'Atenea i FALSE és el nostre
-Volem_detall_atenea <- FALSE
+
+# Nombre dades amb que es farà la mitjana a la comparativa
+cada_quants_mitjana <- 15
+
 
 # A partir d'aquí, en principi, no cal tocar res
 
@@ -18,16 +20,8 @@ require(stringr)
 require(ggplot2)
 require(dplyr)
 
-#Invertim els noms perque es mostri detall del que volem
-if (Volem_detall_atenea) {
-  prev_nom_arxiu_nostre <- nom_arxiu_nostre
-  nom_arxiu_nostre <- nom_arxiu_atenea
-  nom_arxiu_atenea <- prev_nom_arxiu_nostre
-}
-
-
 # Importa txt document
-data <- read.delim2(nom_arxiu_nostre, header = FALSE, 
+data <- read.delim2(nom_arxiu_1, header = FALSE, 
                     sep = "\n", dec = ".")
 
 # Neteja les dades per extreure el total timesteps
@@ -40,30 +34,23 @@ plotData <- data.frame(1:length(raw_rewards)*as.numeric(str_extract(
   raw_timesteps, "[[:digit:]]+.+[[:digit:]]"))[1])
 
 # Extreiem els rewards
-plotData[,2]<- as.numeric(str_extract(raw_rewards, "[[:digit:]]+.+
-                                      [[:digit:]]|-+[[:digit:]]+.+
-                                      [[:digit:]]|[[:digit:]]|-+
-                                      [[:digit:]]"))
+plotData[,2]<- as.numeric(str_extract(raw_rewards, "[[:digit:]]+.+[[:digit:]]|-+[[:digit:]]+.+[[:digit:]]|[[:digit:]]|-+[[:digit:]]"))
 
 # El mateix pero amb el loss
 # El primer valor es NA, perque no te amb que comparar
 raw_loss <- data[str_detect(data[,1], " loss "),]
 
 # Igual que abans xtreiem els loss
-plotData[,3]<- c(NA,as.numeric(str_extract(raw_loss, "[[:digit:]]+.+
-                                           [[:digit:]]|-+[[:digit:]]+.+
-                                           [[:digit:]]|[[:digit:]]|-+
-                                           [[:digit:]]")),
+plotData[,3]<- c(NA,as.numeric(str_extract(raw_loss, "[[:digit:]]+.+[[:digit:]]|-+[[:digit:]]+.+[[:digit:]]|[[:digit:]]|-+[[:digit:]]")),
                  rep(NA, nrow(plotData)-length(raw_loss)-1))
 
 # Reanomenem les columnes del data frame
 names(plotData) <- c("Posicio","Rewards", "Loss")
 
-# Fem la mitjana cada 5 mostres
+# Fem la mitjana cada cada_quants_mitjana mostres
 # Inicialitzem les variables
 n<-0
 suma <- 0
-cadaQuants <-3
 
 # Inicialitzem els comptadors que s'aniran actualitzant
 mean <- c()
@@ -71,10 +58,10 @@ noms <-c()
 
 # Fem un for per a cada fila de les dades
 for (i in 1:nrow(plotData)){
-  if (n==cadaQuants) { # Si toca fer la mitjana
+  if (n==cada_quants_mitjana) { # Si toca fer la mitjana
     # Es fa la mitjana, s'afegeix el nom i 
     # es reinicialitza el comptador i el sumatori
-    mean <- c(mean,suma/cadaQuants)
+    mean <- c(mean,suma/cada_quants_mitjana)
     noms <- c(noms, plotData[i,1])
     n <- 0
     suma <- 0
@@ -105,7 +92,7 @@ LossPlot <- ggplot(plotData[1:250,],aes(x=Posicio, y = Loss)) +
 #Realitzem el mateix procediment pero amb les altres dades
 
 # Importa txt document
-data <- read.delim2(nom_arxiu_atenea, header = FALSE, 
+data <- read.delim2(nom_arxiu_2, header = FALSE, 
                     sep = "\n", dec = ".")
 
 # Neteja les dades per extreure el total timesteps
@@ -119,10 +106,7 @@ plotData <- data.frame(1:length(raw_rewards)*as.numeric(
   str_extract(raw_timesteps, "[[:digit:]]+.+[[:digit:]]"))[1])
 
 # Extreiem els rewards
-plotData[,2]<- as.numeric(str_extract(raw_rewards, "[[:digit:]]+.+
-                                      [[:digit:]]|-+[[:digit:]]+.+
-                                      [[:digit:]]|[[:digit:]]|-+
-                                      [[:digit:]]"))
+plotData[,2]<- as.numeric(str_extract(raw_rewards, "[[:digit:]]+.+[[:digit:]]|-+[[:digit:]]+.+[[:digit:]]|[[:digit:]]|-+[[:digit:]]"))
 
 # Reanomenem les columnes del Data Frame
 names(plotData) <- c("Posicio","Rewards")
@@ -131,7 +115,6 @@ names(plotData) <- c("Posicio","Rewards")
 # Inicialitzem variables
 n<-0
 suma <- 0
-cadaQuants <-3
 
 # Inicialitzem comptadors
 mean <- c()
@@ -139,9 +122,9 @@ noms <-c()
 
 # Repetim el procediment per a cada fila
 for (i in 1:nrow(plotData)){ 
-  if (n==cadaQuants) { # Quan toqui guardar la mitjana
+  if (n==cada_quants_mitjana) { # Quan toqui guardar la mitjana
     # Emmagatzemem mean i noms
-    mean <- c(mean,suma/cadaQuants)
+    mean <- c(mean,suma/cada_quants_mitjana)
     noms <- c(noms, plotData[i,1])
     
     # Resetegem comptadors
@@ -157,21 +140,20 @@ for (i in 1:nrow(plotData)){
 ambMitjanes2 <- data.frame(mean,noms)
 
 #Preparem el grafic per a ser mostrat
-# Agafem nomes els 50 primers valors 
-ambMitjanes1 <-ambMitjanes1[1:50,]
+# Agafem nomes els primers valors
+quants_valors_agafem <- min(nrow(ambMitjanes1), nrow(ambMitjanes2))
+
+ambMitjanes1 <-ambMitjanes1[1:quants_valors_agafem,]
 
 names(ambMitjanes1) <- c("Nou", "noms")
 
 # Afegim els valors de la mitjana del codi d'Atenea
-ambMitjanes1$Atenea <-ambMitjanes2[1:50,1]
+ambMitjanes1$Atenea <-ambMitjanes2[1:quants_valors_agafem,1]
 
 
 # Tornem a invertir els noms per a conservar els colors
-if (Volem_detall_atenea) {
-  names(ambMitjanes1) <- c("Atenea", "noms", "Nou")
-}else {
-  names(ambMitjanes1) <- c("Nou", "noms", "Atenea")
-}
+names(ambMitjanes1) <- c("Nou", "noms", "Atenea")
+
 
 # Creem el gràfic comparatiu
 ComparativaPlot<- ggplot(ambMitjanes1,aes(x=noms)) + # Indiquem les dades utilitzades
@@ -186,6 +168,21 @@ ComparativaPlot<- ggplot(ambMitjanes1,aes(x=noms)) + # Indiquem les dades utilit
   labs(x = "Steps", y = "Reward") 
 
 # Mostrem els grafics
-print(RewardPlot)
-print(LossPlot)
-print(ComparativaPlot)
+if (FALSE){
+  print(RewardPlot)
+  print(LossPlot)
+  print(ComparativaPlot)
+}
+
+# Guardem els gràfics
+ggsave(filename = paste("Grafics/RewardPlot_",nom_arxiu_1,".pdf"), 
+       plot = RewardPlot, device = "pdf", 
+       width = 8.27, height = 4.5, units = c("in"))
+
+ggsave(filename = paste("Grafics/LossPlot_",nom_arxiu_1,".pdf"), 
+       plot = LossPlot, device = "pdf", 
+       width = 8.27, height = 4.5, units = c("in"))
+
+ggsave(filename = paste("Grafics/Comparative_",nom_arxiu_1,"(verd)_vs_",nom_arxiu_2,"(blau).pdf"), 
+       plot = ComparativaPlot, device = "pdf", 
+       width = 8.27, height = 4.5, units = c("in"))
